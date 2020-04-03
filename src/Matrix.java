4cd93fb828b9ded2179b9ab294e86f8828d7bbef
jacobgualtieri <jgualtie@icloud.com>
2020-04-02 21:07:11 -0400

    Imported code from pre-existing java project

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,2 +1,121 @@
-public class Matrix {
+import java.util.LinkedList;
+import java.util.List;
+
+class Matrix {
+    private LinkedList<Vector> columnVectors;
+
+    public Matrix(LinkedList<Vector> vectorList){
+        this.columnVectors = vectorList;
+    }
+
+    public Vector getColumnVector(int columnIndex){
+        return columnVectors.get(columnIndex);
+    }
+
+    public int getNumRows(){
+        return columnVectors.get(0).getcomponentsList().length;
+    }
+
+    public void scaleBy(double scaleFactor){
+        for(int i = 0; i < columnVectors.size(); i++){
+            columnVectors.get(i).scaleBy(scaleFactor);
+        }
+    }
+
+    public Vector getRowVector(int rowIndex){
+        double[] numArray = new double[columnVectors.size()];
+        for (int i = 0; i < columnVectors.size(); i++){
+            numArray[i] = columnVectors.get(i).getComponent(rowIndex);
+        }
+        return new Vector(numArray);
+    }
+
+    public boolean isSquareMatrix(){
+        return this.columnVectors.size() == this.getNumRows();
+    }
+
+    public double determinant(){
+        double result;
+        if(this.isSquareMatrix()){
+            if(this.getNumRows() == 2){
+                double a = columnVectors.get(0).getComponent(0)*columnVectors.get(1).getComponent(1);
+                double b = columnVectors.get(0).getComponent(1)*columnVectors.get(1).getComponent(0);
+                result = a - b;
+            }
+            else{
+                result = 0; // Not yet implemented
+            }
+        }
+        else{
+            System.out.println("Matrix must be square");
+            result = 0;
+        }
+        return result;
+    }
+
+    public static Matrix multiply(Matrix mainMatrix, Matrix otherMatrix){
+        if(mainMatrix.columnVectors.size() == otherMatrix.getNumRows()){
+            LinkedList<Vector> newVectorList = new LinkedList<>();
+            for (int i = 0; i < otherMatrix.columnVectors.size(); i++){
+                double[] resultArray = new double[mainMatrix.getNumRows()];
+                for (int j = 0; j < mainMatrix.getNumRows(); j++){
+                    double sum = 0;
+                    for (int k = 0; k < otherMatrix.columnVectors.get(i).getcomponentsList().length; k++){
+                        for (int l = 0; l < mainMatrix.columnVectors.get(j).getcomponentsList().length; l++){
+                            sum += mainMatrix.columnVectors.get(j).getComponent(l)*otherMatrix.columnVectors.get(i).getComponent(k);
+                        }
+                    }
+                    resultArray[j] = sum;
+                }
+                System.out.println(new Vector(resultArray).toString());
+                newVectorList.add(new Vector(resultArray));
+            }
+            return new Matrix(newVectorList);
+        }
+        else{
+            System.out.println("ERROR: Invalid matrix size");
+            return null;
+        }
+    }
+
+    @Override
+    public String toString(){
+        String result = "[  ";
+        for (int rowNum = 0; rowNum < this.getNumRows(); rowNum++){
+            for(int i = 0; i < columnVectors.size(); i++){
+                result += columnVectors.get(i).getComponent(rowNum) + "  ";
+            }
+            if(rowNum == this.getNumRows()-1){
+                result += "]";
+            }
+            else{
+                result += "]\n[  ";
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args){
+
+        double[] testArray1 = {1,3,5};
+        double[] testArray2 = {2,4,6};
+
+        double[] testArray3 = {20,-5};
+        double[] testArray4 = {-7,6};
+
+        Matrix matrix1 = new Matrix(new LinkedList(List.of(
+                new Vector(testArray1),
+                new Vector(testArray2))));
+
+        Matrix matrix2 = new Matrix(new LinkedList(List.of(
+                new Vector(testArray3),
+                new Vector(testArray4))));
+
+        System.out.println(matrix1.toString()+"\n");
+        System.out.println(matrix2.toString());
+        double det_matrix_2 = matrix2.determinant();
+        System.out.println(det_matrix_2);
+        // Matrix multMatrix = multiply(matrix1, matrix2);
+        // System.out.println(multMatrix.toString());
+    }
 }
--- src/Vector.java
+++ src/Vector.java
@@ -1,2 +1,152 @@
-public class Vector {
+public class Vector{
+    /**
+     * private fields a, b, and c: The x, y, and z components of a three dimensional vector.
+     */
+    private double[] componentsList;
+
+    public Vector(double[] numList){
+        this.componentsList = numList;
+    }
+
+    public Vector(Vector originalVector){
+        this.componentsList = originalVector.componentsList;
+    }
+
+    /**
+     * Get the list of opponents held by this vector object
+     * @return the component list
+     */
+    public double[] getcomponentsList() {
+        return componentsList;
+    }
+
+    /**
+     * @return the list of components
+     */
+    public double getComponent(int componentIndex) {
+        return componentsList[componentIndex];
+    }
+
+    /**
+     * Add the components of another vector, otherVector, to the components of this vector
+     * @param otherVector The second vector that will be added to this vector
+     */
+    public void addVector(Vector otherVector){
+        for(int i = 0; i < componentsList.length; i++){
+            this.componentsList[i] = this.componentsList[i] + otherVector.componentsList[i];
+        }
+    }
+
+    /**
+     * @param o The vector with which the dot product will be performed.
+     * @return The scalar result of the dot product between the vector being referenced and vector o.
+     */
+    public double dotProduct(Vector o){
+        double result = 0;
+        if(o.componentsList.length == this.componentsList.length){
+            for(int i = 0; i < componentsList.length; i++){
+                result += componentsList[i]*o.componentsList[i];
+            }
+        }
+        return result;
+    }
+
+    /**
+     * @param o The vector with which the cross product will be performed.
+     * @return The vector result of the cross product between the vector being referenced and vector o.
+     */
+    public Vector crossProduct(Vector o){
+        double[] temporaryArray = new double[3];
+        if (this.componentsList.length == 3 && o.componentsList.length == 3){
+            temporaryArray[0] = (this.getComponent(1)*o.getComponent(2)) - (this.getComponent(2)*o.getComponent(1));
+            temporaryArray[1] = -1*((this.getComponent(0)*o.getComponent(2)) - (this.getComponent(2)*o.getComponent(0)));
+            temporaryArray[2] = (this.getComponent(0)*o.getComponent(1)) - (this.getComponent(1)*o.getComponent(0));
+        }
+        return new Vector(temporaryArray);
+    }
+
+    /**
+     * @return The scalar magnitude of the vector being referenced.
+     */
+    public double getMagnitude(){
+        double radicand = 0;
+        for(int i = 0; i < componentsList.length; i++){
+            radicand += Math.pow(componentsList[i], 2);
+        }
+        return Math.sqrt(radicand);
+    }
+
+    /**
+     * @param scaleFactor The scalar value by which the vector being referenced will be multiplied.
+     * @return
+     */
+    public void scaleBy(double scaleFactor){
+        for(int i = 0; i < componentsList.length; i++){
+            this.componentsList[i] = scaleFactor*this.componentsList[i];
+        }
+    }
+
+    public static Vector scalarProduct(Vector v, double scaleFactor){
+        double[] newComponentList = new double[v.getcomponentsList().length];
+        for(int i = 0; i < v.componentsList.length; i++){
+            newComponentList[i] = scaleFactor*v.componentsList[i];
+        }
+        return new Vector(newComponentList);
+    }
+
+    /**
+     * @param o The vector onto which this vector is projecting itself.
+     * @return The vector projection of the vector being referenced onto Vector o.
+     */
+    public Vector projection(Vector o){
+        double a_dot_b = this.dotProduct(o);
+        Vector numerator = scalarProduct(o, a_dot_b);
+        double magnitude_squared = Math.pow(o.getMagnitude(),2);
+        double denominator = 1.0/magnitude_squared;
+        return scalarProduct(numerator, denominator);
+    }
+
+    public double comp(Vector o){
+        return this.projection(o).getMagnitude();
+    }
+
+    /**
+     * Determine the sine of the the angle between this vector and the vector v
+     * @param v the second vector associated with the angle theta
+     * @return the sine of theta
+     */
+    public double sineTheta(Vector v){
+        double numerator = this.crossProduct(v).getMagnitude();
+        double denominator = this.getMagnitude()*v.getMagnitude();
+        return numerator/denominator;
+    }
+
+    /**
+     * Determine the cosine of the the angle between this vector and the vector v
+     * @param v the second vector associated with the angle theta
+     * @return the cosine of theta
+     */
+    public double cosineTheta(Vector v){
+        double numerator = this.dotProduct(v);
+        double denominator = this.getMagnitude()*v.getMagnitude();
+        return numerator/denominator;
+    }
+
+    public boolean equals(Vector o){
+        return this.componentsList == o.componentsList;
+    }
+
+    @Override
+    public String toString() {
+        String resultStr = "<";
+        for(int i = 0; i < componentsList.length; i++){
+            if (i < componentsList.length-1){
+                resultStr += this.componentsList[i] + ", ";
+            }
+            else {
+                resultStr += this.componentsList[i] + ">";
+            }
+        }
+        return resultStr;
+    }
 }
